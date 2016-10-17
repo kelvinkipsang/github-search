c865957846b686b5ef9119fc4dfd565da063b991
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-17 09:38:04 +0300

    adding github api

--- demonstration/app/views.py
+++ demonstration/app/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, HttpResponse
-
+import json
+import requests
 # Create your views here.
 
 def index(request):
     return HttpResponse('Hello World!')
+
+def test(request):
+    return HttpResponse('My second view!')
+
+def profile(request):
+    req=requests.get('https://api.github.com/users/kelvinkipsang')
+    content = req.text
+    return HttpResponse(content)
