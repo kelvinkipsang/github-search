ada82e0bd3a4b0518947b57854873afb2ea9c81c
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-17 11:04:10 +0300

    adding css config and files

--- demonstration/app/urls.py
+++ demonstration/app/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from app import views
 
 urlpatterns = [
-  url(r'^$', views.index, name='index'),
+  url(r'^$', views.profile, name='profile'),
   url(r'^test/$', views.test, name='test'),
   url(r'^profile/$', views.profile, name='profile'),
 ]
