723def19e6bd44924304847ab31a5e4ed7bc9721
kelvinkipsang <kelvinkipsang@gmail.com>
2016-10-19 18:27:17 +0300

    changing url and adding template files

--- demonstration/demonstration/urls.py
+++ demonstration/demonstration/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('app.urls')),
+    url(r'^', include('app.urls')),
     url(r'^app/', include('app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
